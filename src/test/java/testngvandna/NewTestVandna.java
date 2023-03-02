520e86991a167953c04fb7ce5ececaa33f1a0d71
VANDANALAPTOP\uvoja <uvojasvi@gmail.com>
2023-03-01 23:16:17 -0600

    Add

---
+++ src/test/java/testngvandna/NewTestVandna.java
@@ -0,0 +1,31 @@
+package testngvandna;
+
+import org.testng.annotations.Test;
+
+public class NewTestVandna {
+  @Test
+  public void NewTest() {
+	  int a = 10;
+	  int b = 20;
+	  System.out.println(a+b);
+  
+  int i = 10;
+  int j = 20;
+  System.out.println(a-b);
+  
+  int e = 80;
+  int g = 60;
+  System.out.println( a + b);
+  
+  int k = 80;
+  int L = 60;
+  System.out.println( a - b);
+  
+  
+  
+  int m = 80;
+  int n = 60;
+  System.out.println( a + b);
+  
+  }
+}
---
+++ src/test/java/testngvandna/Vandnajavaclass.java
@@ -0,0 +1,10 @@
+package testngvandna;
+
+public class Vandnajavaclass {
+
+	public static void main(String[] args) {
+		System.out.println("test vandna");
+
+	}
+
+}
